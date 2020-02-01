979f57505525c62d34e371008e15b6502b3f08ba
andrew <akchen@ualberta.ca>
2020-02-01 16:16:01 -0500

    basic left right move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D pObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pObject = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float v = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(h,v,0);
         
+    }
+
+    void FixedUpdate()
+    {
     }
 }
